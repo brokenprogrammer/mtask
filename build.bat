94e4e310412965ee6d8a5733cb5397e1ff16f3e9
Oskar <brokenprogrammer@gmail.com>
2022-04-21 22:32:32 +0200

    Initial parse of command line arguments.

--- build.bat
+++ build.bat
@@ -11,14 +11,14 @@ where /Q cl.exe || (
     call "!VisualStudio!\VC\Auxiliary\Build\vcvarsall.bat" x64 || exit /b
 )
 
-set COMMON_LIBS= kernel32.lib user32.lib shell32.lib
+set COMMON_LIBS= kernel32.lib user32.lib shell32.lib shlwapi.lib
 
 if "%1" equ "release" (
     set COMPILE_FLAGS= /nologo /O1 /W4 /WX /Gm- /GR- /GS- /EHa- /Oi
-    set LINK_FLAGS= /opt:ref /opt:icf /nodefaultlib /subsystem:windows %COMMON_LIBS% libvcruntime.lib
+    set LINK_FLAGS= /opt:ref /opt:icf /nodefaultlib /subsystem:console %COMMON_LIBS% libvcruntime.lib
 ) else (
-    set COMPILE_FLAGS= /nologo /Od /W4 /WX /MTd /Gm- /GR- /EHa- /Zo /Oi /Zi /FC
-    set LINK_FLAGS= /opt:ref /incremental:no /Debug:full /nodefaultlib /subsystem:windows %COMMON_LIBS% libucrtd.lib libvcruntimed.lib
+    set COMPILE_FLAGS= /nologo /Od /W4 /WX /MTd /Gm- /GR- /GS- /EHa- /Zo /Oi /Zi /FC
+    set LINK_FLAGS= /opt:ref /incremental:no /Debug:full /nodefaultlib /subsystem:console %COMMON_LIBS% libucrtd.lib libvcruntimed.lib
 )
 if not exist build mkdir build
 pushd build
--- src/win32_mtask.cpp
+++ src/win32_mtask.cpp
@@ -5,15 +5,73 @@
 #include <Windows.h>
 #include <processenv.h>
 #include <shellapi.h>
+#include <shlwapi.h>
 
 #include "win32_msvc.cpp"
 
-void __stdcall WinMainCRTStartup()
+static int ArgumentCount;
+static LPWSTR *CommandLineArguments;
+
+static void
+Print(const WCHAR *Message, ...)
 {
 
-    int ArgCount = 0;
-    LPWSTR *CommandLine = CommandLineToArgvW(GetCommandLineW(), &ArgCount);
+    HANDLE ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    LPWSTR CommandLineTest = CommandLine[0];
-    OutputDebugStringW(CommandLineTest);
+    va_list Arguments;
+    va_start(Arguments, Message);
+
+    WCHAR Buffer[1024];
+    DWORD Length = wvsprintfW(Buffer, Message, Arguments);
+
+    if (GetFileType(ConsoleHandle) == FILE_TYPE_CHAR)
+    {
+        DWORD Written;
+        WriteConsoleW(ConsoleHandle, Buffer, Length, &Written, NULL);
+    }
+    else
+    {
+        WriteFile(ConsoleHandle, Buffer, Length, &Written, NULL);
+    }
+    
+    va_end(Arguments);
+}
+
+static void
+Usage(LPCWSTR Command = 0)
+{
+    if (Command)
+    {
+        Print(L"Usage: for command %s\n", Command);
+    }
+    else
+    {
+        Print(L"Usage: Instructions");
+    }
+}
+
+void __stdcall mainCRTStartup()
+{
+
+    CommandLineArguments = CommandLineToArgvW(GetCommandLineW(), &ArgumentCount);
+
+    if (ArgumentCount < 2)
+    {
+        Usage();
+    }
+
+    LPCWSTR Command = CommandLineArguments[1];
+
+    if (StrCmpW(Command, L"help") == 0)
+    {
+        if (ArgumentCount == 3)
+        {
+            Usage(CommandLineArguments[2]);
+        }
+        else
+        {
+            Usage();
+        }
+    }
+
 }
